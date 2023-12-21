3252cb31a6eb292f17fb848b7c2382d4c41d9736
Fabio <fabiomurillo2020@outlook.com>
2023-12-21 08:08:43 -0600

    creating my classes in Gutter and img

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -4,13 +4,14 @@ import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
 import classes from './index.module.scss';
+import Image from 'next/image';
 
 export const HeaderComponent = ({ header }:{ header: Header }) => {
   return (
     <nav className={[classes.header]}>
-      <Gutter>
+      <Gutter className={classes.wrap}>
         <Link href="/">
-        <img src="/logo-black.svg" alt="logo" />
+        <img src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
       </Gutter>
     </nav>
