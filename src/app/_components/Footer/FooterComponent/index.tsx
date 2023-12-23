bacd7f5bbc7c26b7d0a827af0b8dc8e827d77959
Fabio <fabiomurillo2020@outlook.com>
2023-12-23 09:32:34 -0600

    importing elements

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { noHeaderFooterUrls } from "../../../constants";
+import { usePathname } from "next/navigation";
+import { Gutter } from "../../Gutter";
+
 import clases from '.index.module.scss'
 
 const FooterComponent = () => {
