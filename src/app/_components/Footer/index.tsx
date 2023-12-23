481a2fe32f713688cccba44cd61926c11be7024c
Fabio <fabiomurillo2020@outlook.com>
2023-12-23 08:54:43 -0600

    importing my footer component in the index of footer folder

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 import { Footer } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
+import FooterComponent from './FooterComponent'
 
 
 
@@ -19,7 +20,7 @@ export async function Footer() {
 
   return (
    <>
-     <FooterComponent footer = {footer} />
+     <FooterComponent footer={footer} />
    </>
   )
 }
