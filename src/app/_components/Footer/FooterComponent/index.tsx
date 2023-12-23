161840828c04469d8ad9ceac0da17ebcafc7173b
Fabio <fabiomurillo2020@outlook.com>
2023-12-23 12:30:30 -0600

    rendering my footer

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-import { noHeaderFooterUrls } from "../../../constants";
+import { inclusions, noHeaderFooterUrls } from "../../../constants";
 import { usePathname } from "next/navigation";
 import { Gutter } from "../../Gutter";
 
 import clases from '.index.module.scss'
 
 const FooterComponent = () => {
+
+  const pathname = usePathname()
+
   return (
-    <div>Footer</div>
+    <footer className={noHeaderFooterUrls.includes(pathname) ? 
+    clases.hide: ''}>
+      <Gutter>
+        <ul className={clases.inclusions}></ul>
+      </Gutter>
+    </footer>
   )
 }
 
