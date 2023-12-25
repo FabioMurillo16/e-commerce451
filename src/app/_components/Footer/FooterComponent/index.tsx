0d92884cb44e0fa70723c3a58fb167476c0fa769
Fabio <fabiomurillo2020@outlook.com>
2023-12-24 18:28:15 -0600

    rendering my icons of footer

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 import { Gutter } from "../../Gutter";
 
 import clases from './index.module.scss'
+import Image from 'next/image';
 
 const FooterComponent = () => {
 
@@ -16,7 +17,21 @@ const FooterComponent = () => {
     <footer className={noHeaderFooterUrls.includes(pathname) ? 
     clases.hide: ''}>
       <Gutter>
-        <ul className={clases.inclusions}></ul>
+        {/* here im exporting from inclusion all the data and 
+        icons inside the tag ul and rendering using .map*/}
+        <ul className={clases.inclusion}>
+          {inclusions.map((inclusion, index) => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.description}
+                width={36}
+                height={36}
+                className={clases.icon}
+              />
+            </li>
+          ))}
+        </ul>
       </Gutter>
     </footer>
   )
