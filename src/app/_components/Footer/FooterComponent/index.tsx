1ca1bed9d1a672523ef7f39b0fcb57476cbd58ce
Fabio <fabiomurillo2020@outlook.com>
2023-12-25 19:11:17 -0600

    adding footer and description of my inclusions, and my logo in the footer

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -8,6 +8,7 @@ import { Gutter } from "../../Gutter";
 
 import clases from './index.module.scss'
 import Image from 'next/image';
+import Link from 'next/link';
 
 const FooterComponent = () => {
 
@@ -19,7 +20,7 @@ const FooterComponent = () => {
       <Gutter>
         {/* here im exporting from inclusion all the data and 
         icons inside the tag ul and rendering using .map*/}
-        <ul className={clases.inclusion}>
+        <ul className={clases.inclusions}>
           {inclusions.map((inclusion, index) => (
             <li key={inclusion.title}>
               <Image
@@ -29,10 +30,28 @@ const FooterComponent = () => {
                 height={36}
                 className={clases.icon}
               />
+
+              <h5 className={clases.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
             </li>
           ))}
         </ul>
+        
       </Gutter>
+
+      <div className={clases.footer}>
+
+        <Gutter>
+          <div className={clases.wrap}>
+            <Link href='/link'>
+              <Image src="/logo-white.svg" alt='logo' width={170} 
+              height={70} />
+            </Link>
+          </div>
+        </Gutter>
+
+      </div>
+
     </footer>
   )
 }
